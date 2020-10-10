d8086104468e464e95e35229745ecf6403f822b6
Jake Burzlaff <jake.bilbao.04.06@gmail.com>
2020-10-09 21:18:26 -0600

    updated schema.sql with to create db and tables

--- employee-directory.js
+++ employee-directory.js
@@ -8,7 +8,7 @@ const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: process.env.password
+    password: process.env.password;
 });
 
 connection.connect(function(err) {

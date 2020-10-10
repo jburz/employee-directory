305f26f792b9564dd28ca2cae3646c5ab59ed4a0
Jake Burzlaff <jake.bilbao.04.06@gmail.com>
2020-10-09 19:37:24 -0600

    added inquirer and mysql as required packages, initialized local mysql connection

--- employee-directory.js
+++ employee-directory.js
@@ -1,4 +1,20 @@
 //required packages
 const env = require ('dotenv').config();
+const inquirer = require('inquirer');
+const mysql = require('mysql');
 
-console.log(process.env.password);
+//connect to database
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: process.env.password
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log('hit');
+    connection.end();
+})
+
+

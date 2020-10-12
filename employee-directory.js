4aa85a9ea0ec8e6e27585961a2aa9336c16c28fc
Jake Burzlaff <jake.bilbao.04.06@gmail.com>
2020-10-11 19:03:20 -0600

    added console.table to dependencies, added new query to return departments when view employees by department is selected

--- employee-directory.js
+++ employee-directory.js
@@ -2,6 +2,7 @@
 const env = require ('dotenv').config();
 const inquirer = require('inquirer');
 const mysql = require('mysql');
+const cTable = require('console.table');
 
 //connect to database
 const connection = mysql.createConnection({
@@ -14,14 +15,90 @@ const connection = mysql.createConnection({
 
 connection.connect(function(err) {
     if (err) throw err;
+    console.log('Employee Directory');
+    
+    console.log("=====================================================================================================================================");
+    console.log("=        ===================  ====================================      =============================================================");
+    console.log("=  =========================  ===================================   ==   ============================================================");
+    console.log("=  =========================  ===================================  ====  ========================================  ==================");
+    console.log("=  ========  =  = ===    ===  ===   ===  =  ===   ====   ========  =========   ===  = ====   ===  =   ====   ===    ===   ===  =   ==");
+    console.log("=      ====        ==  =  ==  ==     ==  =  ==  =  ==  =  =======  ========  =  ==     ==  =  ==    =  ==  =  ===  ===     ==    =  =");
+    console.log("=  ========  =  =  ==  =  ==  ==  =  ===    ==     ==     =======  ===   ==     ==  =  ==     ==  ==========  ===  ===  =  ==  ======");
+    console.log("=  ========  =  =  ==    ===  ==  =  =====  ==  =====  ==========  ====  ==  =====  =  ==  =====  ========    ===  ===  =  ==  ======");
+    console.log("=  ========  =  =  ==  =====  ==  =  ==  =  ==  =  ==  =  =======   ==   ==  =  ==  =  ==  =  ==  =======  =  ===  ===  =  ==  ======");
+    console.log("=        ==  =  =  ==  =====  ===   ====   ====   ====   =========      ====   ===  =  ===   ===  ========    ===   ===   ===  ======");
+    console.log("=====================================================================================================================================");
     start();
 });
 
 function start() {
-    console.log('Employee Directory');
-    connection.query("SELECT department.name AS Departments, sum(role.salary) AS Budget FROM department JOIN role ON department.id=role.department_id GROUP BY Departments;", function(err, res) {
+    inquirer
+        .prompt([
+           {
+               type: "list",
+               message: "Please select an option from the list:",
+               name: "mainMenu",
+               choices: [
+                   "View All Employees",
+                   "View All Employees By Department",
+                   "View All Employees By Manager",
+                   "Add Employee",
+                   "Remove Employee",
+                   "Update Employee Role",
+                   "Update Employee Manager",
+                   "Exit"
+               ]
+           }
+        ]).then(function(data) {
+            if (data.mainMenu === "View All Employees") {
+                viewAll();
+            }
+            else if(data.mainMenu === "View All Employees By Department") {
+                viewAllByDept();
+            }
+            else if(data.mainMenu === "Exit") {
+                console.log('Goodbye');
+                connection.end();
+            }
+        });
+}
+
+function viewAll() {
+    connection.query("SELECT employee.id AS ID, employee.first_name AS FirstName, employee.last_name AS LastName, role.title AS Position, department.name AS Department, role.salary AS Salary, CONCAT(Manager.first_name, \" \", Manager.last_name) AS Manager FROM employee JOIN role ON employee.role_id=role.id JOIN department ON role.department_id=department.id LEFT JOIN employee AS Manager ON employee.manager_id=Manager.id ORDER BY ID;", function(err, res) {
         if (err) throw err;
         console.table(res);
+        start();
     })
-    connection.end();
+}
+
+function viewAllByDept() {
+    connection.query("SELECT department.name AS Departments FROM department JOIN role ON department.id=role.department_id GROUP BY Departments;", function(err, res) {
+        if (err) throw err;
+        console.table(res);
+        start();
+    });
+    // inquirer
+    //     .prompt([
+
+    //     ])
+}
+
+function viewAllByMgr() {
+
+}
+
+function addEmployee() {
+
+}
+
+function removeEmployee() {
+
+}
+
+function updateRole() {
+
+}
+
+function updateMgr() {
+
 }
